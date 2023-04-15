2ed2e4ae92a063803bd4afc4b429aebda41e80da
JefersonNakayama <nakayamajeff@gmail.com>
2023-04-15 11:48:41 -0300

    Atividade 03-JefersonNakayama

--- main.py
+++ main.py
@@ -1,38 +1,41 @@
-class No:
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+        self.size = 0
+        
+    def inserir_30(self):
+        for i in range(1, 31):
+            self.inserir(i)
+
+    def inserir(self, valor):
+        if self.root is None:
+            self.root = Node(valor)
+            self.size += 1
+        else:
+            self._inserir(valor, self.root)
+
+    def _inserir(self, valor, node_atual):
+        if valor < node_atual.valor:
+            if node_atual.esquerda is None:
+                node_atual.esquerda = Node(valor)
+                self.size += 1
+            else:
+                self._inserir(valor, node_atual.esquerda)
+        elif valor > node_atual.valor:
+            if node_atual.direita is None:
+                node_atual.direita = Node(valor)
+                self.size += 1
+            else:
+                self._inserir(valor, node_atual.direita)
+        else:
+            # Ignorar valores duplicados
+            pass
+
+    def quantidade(self):
+        return self.size
+
+class Node:
     def __init__(self, valor):
         self.valor = valor
         self.esquerda = None
         self.direita = None
-        
-class BinaryTree:
-    def __init__(self):
-        self.raiz = None
-        self.qtd_nos = 0
-        
-    def inserir(self, valor):
-        novo_no = No(valor)
-        
-        if self.raiz is None:
-            self.raiz = novo_no
-        else:
-            atual = self.raiz
-            
-            while True:
-                if valor < atual.valor:
-                    if atual.esquerda is None:
-                        atual.esquerda = novo_no
-                        break
-                    else:
-                        atual = atual.esquerda
-                else:
-                    if atual.direita is None:
-                        atual.direita = novo_no
-                        break
-                    else:
-                        atual = atual.direita
-        
-        self.qtd_nos += 1
-        
-    def quantidade(self):
-        return self.qtd_nos
-
